2fa8237e2a3b2ff42d5d7c586a3b1c1642a2d706
Bob Layon Salas <salabob06@k2ac.intern>
2025-02-21 12:48:43 +0100

    #24,#26,#27: created Company, OfferedPresentaton and PlannedPresentation classes

--- src/main/java/itf221/gvi/boom/data/Company.java
+++ src/main/java/itf221/gvi/boom/data/Company.java
@@ -1,5 +1,13 @@
 package itf221.gvi.boom.data;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * Holds Company data.
+ */
+@AllArgsConstructor
+@Data
 public class Company {
     String name;
     OfferedPresentation offeredPresentations;
--- src/main/java/itf221/gvi/boom/data/PlannedPresentation.java
+++ src/main/java/itf221/gvi/boom/data/PlannedPresentation.java
@@ -1,5 +1,18 @@
 package itf221.gvi.boom.data;
 
-public class PlannedPresentation {
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+import java.util.List;
+
+/**
+ * Holds data for scheduled presentations.
+ */
+@AllArgsConstructor
+@Data
+public class PlannedPresentation {
+    private char timeslot;
+    private Room room;
+    private OfferedPresentation offeredPresentation;
+    private List<Student> attendees;
 }
