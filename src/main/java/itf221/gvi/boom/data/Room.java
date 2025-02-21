40ac1802202d19c1912ad55cb6cbaa03a632c1a4
Natalie Schroiff <schrnat04@k2ac.intern>
2025-02-21 11:43:13 +0100

    #25: added Room class

---
+++ src/main/java/itf221/gvi/boom/data/Room.java
@@ -0,0 +1,13 @@
+package itf221.gvi.boom.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Room {
+    String roomNumber;
+    int capacity;
+}
