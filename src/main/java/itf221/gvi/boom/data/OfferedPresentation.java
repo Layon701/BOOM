8de069d397be67a1f2d74259350c20bb0981be12
Bob Layon Salas <salabob06@k2ac.intern>
2025-02-21 12:48:43 +0100

    #24,#26,#27: created Company, OfferedPresentaton and PlannedPresentation classes

--- src/main/java/itf221/gvi/boom/data/OfferedPresentation.java
+++ src/main/java/itf221/gvi/boom/data/OfferedPresentation.java
@@ -1,4 +1,25 @@
 package itf221.gvi.boom.data;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import java.util.List;
+
+/**
+ * Holds data for a company's offered presentations. Acts as a presentation blueprint for itf221.gvi.boom.data.PlannedPresentations.
+ */
+@AllArgsConstructor
+@Data
 public class OfferedPresentation {
+    private int id;
+    private int minCapacity;
+    private int maxCapacity;
+    private String specialty;
+    private char earliestTime;
+    private String companyName;
+    private List<PlannedPresentation> plannedPresentations;
+
+    public String getTitle() {
+        return String.format("%s: %s", this.getCompanyName(), this.getSpecialty());
+    }
 }
+
