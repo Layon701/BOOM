f19bda2d1a41251c7c7c0e9483312bdc23f582b2
Bob Layon Salas <salabob06@k2ac.intern>
2025-02-21 13:07:21 +0100

    #28: Added BoomData class

---
+++ src/main/java/itf221/gvi/boom/data/BoomData.java
@@ -0,0 +1,19 @@
+package itf221.gvi.boom.data;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Holds all relevant BOOM data.
+ */
+@AllArgsConstructor
+@Data
+public class BoomData {
+    private Map<String, Room> rooms;
+    private List<Company> companies;
+    private Map<String, Student> students;
+}
