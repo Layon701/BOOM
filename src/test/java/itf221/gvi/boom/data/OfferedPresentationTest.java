149ad2726d42843ca9fdd7f6cd1b36a2317a0c1d
Bob Layon Salas <salabob06@k2ac.intern>
2025-02-21 13:03:21 +0100

    #24: added test for getTitle

---
+++ src/test/java/itf221/gvi/boom/data/OfferedPresentationTest.java
@@ -0,0 +1,27 @@
+package itf221.gvi.boom.data;
+
+import itf221.gvi.boom.io.reader.XlsxReader;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OfferedPresentationTest {
+    /**
+     * Tests the getTitle Method.
+     */
+    @Test
+    public void getTitleTest() throws IOException {
+        OfferedPresentation offeredPresentation =
+                new OfferedPresentation(1, 5, 20,
+                        "Polizeikommisar*in", 'A', "Polizei", new ArrayList<>());
+
+        String expectedTitle = "Polizei: Polizeikommisar*in";
+        assertEquals(expectedTitle, offeredPresentation.getTitle());
+    }
+}
