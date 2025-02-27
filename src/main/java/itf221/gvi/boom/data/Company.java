38ab99e346df3fcba39052b90178aa1d3170c057
Natalie Schroiff <schrnat04@k2ac.intern>
2025-02-27 08:20:03 +0100

    #26: added list

--- src/main/java/itf221/gvi/boom/data/Company.java
+++ src/main/java/itf221/gvi/boom/data/Company.java
@@ -3,12 +3,14 @@ package itf221.gvi.boom.data;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.List;
+
 /**
  * Holds Company data.
  */
 @AllArgsConstructor
 @Data
 public class Company {
-    String name;
-    OfferedPresentation offeredPresentations;
+    private String name;
+    private List<OfferedPresentation> offeredPresentations;
 }
