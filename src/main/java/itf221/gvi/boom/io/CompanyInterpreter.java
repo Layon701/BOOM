d85ee9d600b52c46e330be1decd17cc0ce069af3
Yannick Roscher <roscyan19@k2ac.intern>
2025-03-06 09:01:53 +0100

    #18, #19, #20: added Interpreter classes

---
+++ src/main/java/itf221/gvi/boom/io/CompanyInterpreter.java
@@ -0,0 +1,13 @@
+package itf221.gvi.boom.io;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import itf221.gvi.boom.data.Company;
+
+public class CompanyInterpreter {
+	public static List<Company> interpret(List<String> data)
+	{
+		return new ArrayList<Company>();
+	}
+}
---
+++ src/main/java/itf221/gvi/boom/io/RoomInterpreter.java
@@ -0,0 +1,13 @@
+package itf221.gvi.boom.io;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import itf221.gvi.boom.data.Room;
+
+public class RoomInterpreter {
+	public static List<Room> interpret(List<String> data)
+	{
+		return new ArrayList<Room>();
+	}
+}
