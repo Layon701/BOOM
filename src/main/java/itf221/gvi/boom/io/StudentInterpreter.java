08a11fbb82d815781f98477e75434196b5f4dcfa
Yannick Roscher <roscyan19@k2ac.intern>
2025-03-06 09:01:53 +0100

    #18, #19, #20: added Interpreter classes

---
+++ src/main/java/itf221/gvi/boom/io/StudentInterpreter.java
@@ -0,0 +1,13 @@
+package itf221.gvi.boom.io;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import itf221.gvi.boom.data.Student;
+
+public class StudentInterpreter {
+	public static List<Student> interpret(List<String> data)
+	{
+		return new ArrayList<Student>();
+	}
+}
