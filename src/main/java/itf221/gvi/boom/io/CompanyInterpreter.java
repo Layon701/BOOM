bab3cbe353a2511d7bb3d05a4d4276d1d5caba0f
Yannick Roscher <roscyan19@k2ac.intern>
2025-03-07 09:33:58 +0100

    #18, implemented functoin to interpret imported company data

--- src/main/java/itf221/gvi/boom/io/CompanyInterpreter.java
+++ src/main/java/itf221/gvi/boom/io/CompanyInterpreter.java
@@ -1,13 +1,43 @@
 package itf221.gvi.boom.io;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.ArrayList;
 
 import itf221.gvi.boom.data.Company;
+import itf221.gvi.boom.data.OfferedPresentation;
 
 public class CompanyInterpreter {
-	public static List<Company> interpret(List<String> data)
+	public static List<Company> interpret(List<List<String>> data)
 	{
-		return new ArrayList<Company>();
+		HashMap<String, Company> commap = new HashMap<String, Company>();
+
+		for(List<String> ls : data)
+		{	
+			if(ls.size() != 6)
+			{
+				continue;
+			}
+
+			String name = ls.get(1);
+			Company currentCompany;
+
+			if(commap.containsKey(name))
+			{
+				currentCompany = commap.get(name);
+			}
+			else
+			{
+				currentCompany =new Company(name, new ArrayList<OfferedPresentation>());
+				commap.put(name, currentCompany);
+			}
+
+			//adds new OfferedPrsentation to company
+			currentCompany.getOfferedPresentations().add(new OfferedPresentation(Integer.parseInt(ls.get(0)),0,Integer.parseInt(ls.get(3)),ls.get(2),ls.get(5).charAt(0), name));
+		}
+
+		List<Company> companies = new ArrayList<Company>(commap.values());
+		return companies;
 	}
 }
