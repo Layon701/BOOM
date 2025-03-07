3c308575caa7db9f8d430ecd5f82b5d14f9f4bb6
Yannick Roscher <roscyan19@k2ac.intern>
2025-03-07 10:29:55 +0100

    #19, implemented function for interpretation of student data

--- src/main/java/itf221/gvi/boom/io/StudentInterpreter.java
+++ src/main/java/itf221/gvi/boom/io/StudentInterpreter.java
@@ -1,13 +1,48 @@
 package itf221.gvi.boom.io;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
 
+import itf221.gvi.boom.data.Company;
+import itf221.gvi.boom.data.OfferedPresentation;
 import itf221.gvi.boom.data.Student;
 
 public class StudentInterpreter {
-	public static List<Student> interpret(List<String> data)
-	{
-		return new ArrayList<Student>();
-	}
+    public static List<Student> interpret(List<List<String>> data, List<Company> companies) {
+        //make Map of presentations
+        Map<Integer, OfferedPresentation> presentationMap = new HashMap<Integer, OfferedPresentation>();
+        for (Company company : companies) {
+            for (OfferedPresentation presentation : company.getOfferedPresentations()) {
+                presentationMap.put(presentation.getId(), presentation);
+            }
+        }
+
+        //
+        ArrayList<Student> students = new ArrayList<Student>();
+        int studentId = 0;
+        for (List<String> row : data) {
+            studentId++;
+            if(row.size() != 9)
+            {
+                continue;
+            }
+
+            List<OfferedPresentation> wishes = new ArrayList<OfferedPresentation>();
+            for(int columnIndex = 3; columnIndex < 9; columnIndex++)
+            {
+                Integer wishIndex = Integer.parseInt(row.get(columnIndex));
+                OfferedPresentation presentation = presentationMap.get(wishIndex);
+                wishes.add(presentation);
+            }
+
+            String schoolClass = row.get(0);
+            String name = row.get(1);
+            String surname = row.get(2);
+
+            students.add(new Student(wishes, surname, name,schoolClass, studentId));
+        }
+        return students;
+    }
 }
