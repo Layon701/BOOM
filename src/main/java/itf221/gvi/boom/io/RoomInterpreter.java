6a9a171ca15387d86f761299bea78d1b81b9d4fe
Yannick Roscher <roscyan19@k2ac.intern>
2025-03-07 11:05:30 +0100

    #20, increased readability

--- src/main/java/itf221/gvi/boom/io/RoomInterpreter.java
+++ src/main/java/itf221/gvi/boom/io/RoomInterpreter.java
@@ -9,23 +9,23 @@ import itf221.gvi.boom.data.Room;
  * Interpreter for the room Excel-sheet
  */
 public class RoomInterpreter {
-	/**
-	 * Creates a list of Room objects
-	 * @param data List<List<String>> (from the Xlsx-Reader)
-	 * @return List<Room>
-	 */
-	public static List<Room> interpret(List<List<String>> data)
-	{
-		ArrayList<Room> rooms = new ArrayList<Room>();
-		for(List<String> ls : data)
-		{
-			if(ls.size() != 2)
-			{
-				continue;
-			}
+    /**
+     * Creates a list of Room objects
+     *
+     * @param data List<List<String>> (from the Xlsx-Reader)
+     * @return List<Room>
+     */
+    public static List<Room> interpret(List<List<String>> data) {
+        ArrayList<Room> rooms = new ArrayList<Room>();
+        for (List<String> row : data) {
+            if (row.size() != 2) {
+                continue;
+            }
 
-			rooms.add(new Room(ls.get(0), Integer.parseInt(ls.get(1))));
-		}
-		return rooms;
-	}
+            String name = row.get(0);
+            int capacity = Integer.parseInt(row.get(1));
+            rooms.add(new Room(name, capacity));
+        }
+        return rooms;
+    }
 }
