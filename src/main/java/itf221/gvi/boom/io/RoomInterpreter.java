bf3784021ae9e9a2ac61f894838c16131e572fa3
Yannick Roscher <roscyan19@k2ac.intern>
2025-03-07 09:34:35 +0100

    #20, implemented functoin to interpret imported room data

--- src/main/java/itf221/gvi/boom/io/RoomInterpreter.java
+++ src/main/java/itf221/gvi/boom/io/RoomInterpreter.java
@@ -6,8 +6,18 @@ import java.util.ArrayList;
 import itf221.gvi.boom.data.Room;
 
 public class RoomInterpreter {
-	public static List<Room> interpret(List<String> data)
+	public static List<Room> interpret(List<List<String>> data)
 	{
-		return new ArrayList<Room>();
+		ArrayList<Room> rooms = new ArrayList<Room>();
+		for(List<String> ls : data)
+		{
+			if(ls.size() != 2)
+			{
+				continue;
+			}
+
+			rooms.add(new Room(ls.get(0), Integer.parseInt(ls.get(1))));
+		}
+		return rooms;
 	}
 }
