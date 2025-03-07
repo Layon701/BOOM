53256ae9cd235e9d077d19d5cc9ca904e7937d7d
Yannick Roscher <roscyan19@k2ac.intern>
2025-03-07 09:25:42 +0100

    #18, #19, modified Constructors to exclude data that is not imported from files

--- src/main/java/itf221/gvi/boom/data/Student.java
+++ src/main/java/itf221/gvi/boom/data/Student.java
@@ -3,6 +3,7 @@ package itf221.gvi.boom.data;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,4 +18,14 @@ public class Student {
     private String name;
     private String schoolClass;
     private int id;
+
+    public Student(List<OfferedPresentation> offeredPresentations, String surname, String name, String schoolClass, int id)
+    {
+        this.plannedPresentations = new ArrayList<PlannedPresentation>();
+        this.offeredPresentations = offeredPresentations;
+        this.surname = surname;
+        this.name = name;
+        this.schoolClass = schoolClass;
+        this.id = id;
+    }
 }
