b33afe471a6f852e912679e87c9aabd1d209361f
Natalie Schroiff <natalie.schroiff@outlook.com>
2025-03-29 10:27:18 +0100

    #33: added methods, still needs real logic from BE to work as intended

--- src/main/java/itf221/gvi/boom/gui/controller/LoadingController.java
+++ src/main/java/itf221/gvi/boom/gui/controller/LoadingController.java
@@ -1,4 +1,45 @@
 package itf221.gvi.boom.gui.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
+/**
+ * Controller class for managing the loading view of the application.
+ */
 public class LoadingController {
+
+    @FXML
+    Button export_view_button;
+
+    /**
+     * Initializes the 'loading' process by switching to the export view.
+     * @param event  The ActionEvent triggered by the user action.
+     * @throws IOException If there is an error while loading the view.
+     */
+    @FXML
+    private void initLoading(ActionEvent event) throws IOException {
+       switchToExportView(event);
+    }
+
+    /**
+     * Switches to the export view. It loads the export-view FXML file and sets it as the scene for the current stage.
+     * @param event The ActionEvent triggered by the user action.
+     * @throws IOException If there is an error while loading the FXML file.
+     */
+    private void switchToExportView(ActionEvent event) throws IOException {
+        // TODO: No real logic for now, just a button to see the next view
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/itf221/gvi/boom/fxml/export-view.fxml"));
+        Parent root = fxmlLoader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
 }
