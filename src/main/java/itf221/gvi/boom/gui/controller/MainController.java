95573b658570ca84eefc15e1cfb18f1ad0196146
Benny mahaut <bennymahaut@live.de>
2025-04-04 08:32:28 +0200

    #33: starting method and run algorithm

--- src/main/java/itf221/gvi/boom/gui/controller/MainController.java
+++ src/main/java/itf221/gvi/boom/gui/controller/MainController.java
@@ -1,5 +1,8 @@
 package itf221.gvi.boom.gui.controller;
 
+import itf221.gvi.boom.RoomManagementUnit;
+import itf221.gvi.boom.data.BoomData;
+import itf221.gvi.boom.io.IOManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,6 +17,8 @@ import javafx.stage.Stage;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Objects;
 
 /**
@@ -49,6 +54,24 @@ public class MainController {
     @FXML
     private void initStart(ActionEvent event) throws IOException {
         switchToLoadingView(event);
+        runDistributionAlg();
+    }
+
+    @FXML
+    private void runDistributionAlg() {
+        IOManager ioManager = new IOManager(
+                Paths.get(sw_import_field.getText()),
+                Paths.get(ul_import_field.getText()),
+                Paths.get(rp_import_field.getText()),
+                getDownloadFolderPath());
+        BoomData boomData = ioManager.readFiles();
+        RoomManagementUnit roomManagementUnit = new RoomManagementUnit();
+        roomManagementUnit.execute(boomData);
+    }
+
+    public Path getDownloadFolderPath() {
+        String userHome = System.getProperty("user.home");
+        return Paths.get(userHome, "Downloads");
     }
 
     /**
