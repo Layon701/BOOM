3e93efa90f2c3281a4c16d10e7c27c8705c365e0
Natalie Schroiff <natalie.schroiff@outlook.com>
2025-04-10 05:57:18 +0200

    #33: added success toast

--- src/main/java/itf221/gvi/boom/gui/controller/ExportController.java
+++ src/main/java/itf221/gvi/boom/gui/controller/ExportController.java
@@ -1,4 +1,36 @@
 package itf221.gvi.boom.gui.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * Controller class for managing the export view of the application.
+ */
 public class ExportController {
+
+    @FXML
+    private AnchorPane toastPane;
+
+    /**
+     * Called automatically after the FXML is loaded.
+     */
+    @FXML
+    public void initialize() {
+        showSuccessToast();
+    }
+
+    /**
+     * Shows the success toast message.
+     */
+    private void showSuccessToast() {
+        toastPane.setVisible(true);
+    }
+
+    /**
+     * Closes the toast when the user clicks the "x" button.
+     */
+    @FXML
+    public void closeToast() {
+        toastPane.setVisible(false);
+    }
 }
