1a0947924c9dbbbf4cf7ac5756eef3921109aaff
Benny mahaut <bennymahaut@live.de>
2025-04-10 08:52:37 +0200

    #33: added completion score to toast in export view

--- src/main/java/itf221/gvi/boom/gui/controller/ExportController.java
+++ src/main/java/itf221/gvi/boom/gui/controller/ExportController.java
@@ -1,7 +1,12 @@
 package itf221.gvi.boom.gui.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Controller class for managing the export view of the application.
@@ -11,12 +16,16 @@ public class ExportController {
     @FXML
     private AnchorPane toastPane;
 
+    @FXML
+    private Label completionScoreToast;
+
     /**
      * Called automatically after the FXML is loaded.
      */
     @FXML
     public void initialize() {
         showSuccessToast();
+        completionScoreToast.setText("✔ Erfolg! Erfüllungsscore: " + MainController.completionScore + "%");
     }
 
     /**
@@ -32,5 +41,19 @@ public class ExportController {
     @FXML
     public void closeToast() {
         toastPane.setVisible(false);
+    }
+
+    @FXML
+    private void openDocumentation() {
+        File pdfFile = new File("src/main/resources/itf221/gvi/boom/documentation/Benutzerdoku.pdf");
+        if (pdfFile.exists()) {
+            try {
+                Desktop.getDesktop().open(pdfFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("File not found!");
+        }
     }
 }
