45ad3f7905231c8a62e7b04021d31e2fd25be3d6
Benny mahaut <bennymahaut@live.de>
2025-04-11 12:04:13 +0200

    #80: executable jar works, warning can be ignored for now

--- src/main/java/itf221/gvi/boom/Main.java
+++ src/main/java/itf221/gvi/boom/Main.java
@@ -6,21 +6,8 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/itf221/gvi/boom/fxml/main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-        stage.setTitle("BOOM");
-        Image icon = new Image(String.valueOf(getClass().getResource("/itf221/gvi/boom/images/boom.png")));
-        stage.getIcons().add(icon);
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.show();
-    }
-
+public class Main {
     public static void main(String[] args) {
-        launch();
+        GUI.launch(GUI.class, args);
     }
 }
