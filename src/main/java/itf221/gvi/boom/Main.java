e4d556d65268b1db38239780a4f2a8cdd5eaa3e6
Benny mahaut <bennymahaut@live.de>
2025-04-11 12:34:10 +0200

    #33: refactored -> removed unused imports

--- src/main/java/itf221/gvi/boom/Main.java
+++ src/main/java/itf221/gvi/boom/Main.java
@@ -1,11 +1,5 @@
 package itf221.gvi.boom;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.stage.Stage;
-
 public class Main {
     public static void main(String[] args) {
         GUI.launch(GUI.class, args);
