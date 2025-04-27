fd3fc78b0f0343145a332f27817a916a17653763
Natalie Schroiff <natalie.schroiff@outlook.com>
2025-04-27 10:11:57 +0200

    #64: added javadoc for pdf writer

--- src/main/java/itf221/gvi/boom/io/writer/PdfWriter.java
+++ src/main/java/itf221/gvi/boom/io/writer/PdfWriter.java
@@ -8,55 +8,70 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
 
+/**
+ * This class is intended for future extension to generate PDF documents.
+ */
 public class PdfWriter implements FileWriter{
 
+    /**
+     * Writes a student plan to the specified file path.
+     * @param path the file path to write to.
+     * @param students the list of students.
+     * @throws IOException if an I/O error occurs.
+     */
     public void writeStudentPlan(Path path, List<Student> students) throws IOException{
         // open file writer
-
         // write title
-
         for(Student student : students)
         {
             writeStudent(student);
         }
-
         // close file writer
     }
 
+    /**
+     * Writes a presentation attendance list to the specified file path.
+     * @param path the file path to write to.
+     * @param presentations the list of planned presentations.
+     * @throws IOException if an I/O error occurs.
+     */
     public void writePresentationAttendance(Path path, List<PlannedPresentation> presentations) throws IOException{
         // open file writer
-
         // write title
-
         for(PlannedPresentation presentation : presentations)
         {
             writePresentation(presentation);
         }
-
         // close file writer
     }
 
+    /**
+     * Writes a room timetable plan to the specified file path.
+     * @param path the file path to write to.
+     * @param presentations the list of offered presentations.
+     * @throws IOException if an I/O error occurs.
+     */
     public void writeRoomTimetable(Path path, List<OfferedPresentation> presentations) throws IOException{
         // open file writer
 
         // write title
-
-        for(OfferedPresentation presentation : presentations)
-        {
-
-        }
-
+        for(OfferedPresentation presentation : presentations) { }
         // close file writer
     }
 
-    protected void writeStudent(Student student) throws IOException
-    {
+    /**
+     * Helper method to write a single student's information into the PDF.
+     *
+     * @param student the student to write.
+     * @throws IOException if an I/O error occurs.
+     */
+    protected void writeStudent(Student student) throws IOException { }
 
-    }
-
-    protected void writePresentation(PlannedPresentation presentation) throws IOException
-    {
-
-    }
-
+    /**
+     * Helper method to write a single planned presentation's information into the PDF.
+     *
+     * @param presentation the planned presentation to write.
+     * @throws IOException if an I/O error occurs.
+     */
+    protected void writePresentation(PlannedPresentation presentation) throws IOException { }
 }
