b6fda96d68049658763a5ea5495ed9c7431bba7a
Natalie Schroiff <natalie.schroiff@outlook.com>
2025-04-27 10:48:48 +0200

    #64: added javadoc for main class

--- src/main/java/itf221/gvi/boom/Main.java
+++ src/main/java/itf221/gvi/boom/Main.java
@@ -1,6 +1,14 @@
 package itf221.gvi.boom;
 
+/**
+ * Entry point for launching the BOOM application.
+ */
 public class Main {
+    /**
+     * Main method that starts the JavaFX application.
+     *
+     * @param args the command-line arguments.
+     */
     public static void main(String[] args) {
         GUI.launch(GUI.class, args);
     }
