fb2417bf1876c0d17c6c44b68ebc4e027d83f3a3
Natalie Schroiff <natalie.schroiff@outlook.com>
2025-04-27 10:22:11 +0200

    #64: added javadoc for empty file exception

--- src/main/java/itf221/gvi/boom/exceptions/EmptyFileException.java
+++ src/main/java/itf221/gvi/boom/exceptions/EmptyFileException.java
@@ -1,5 +1,8 @@
 package itf221.gvi.boom.exceptions;
 
+/**
+ * Exception thrown to indicate that a file is unexpectedly empty.
+ */
 public class EmptyFileException extends RuntimeException {
     public EmptyFileException(String message) {
         super(message);
