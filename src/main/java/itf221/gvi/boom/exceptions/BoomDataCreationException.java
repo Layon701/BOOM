1af3917a57424ae989a4efd16dac154de178fc4f
Natalie Schroiff <natalie.schroiff@outlook.com>
2025-04-27 10:20:03 +0200

    #64: added javadoc for boom data creation exception

--- src/main/java/itf221/gvi/boom/exceptions/BoomDataCreationException.java
+++ src/main/java/itf221/gvi/boom/exceptions/BoomDataCreationException.java
@@ -1,5 +1,8 @@
 package itf221.gvi.boom.exceptions;
 
+/**
+ * Exception thrown to indicate an error occurred during the creation of Boom data.
+ */
 public class BoomDataCreationException extends RuntimeException {
     public BoomDataCreationException(String message) {
         super(message);
