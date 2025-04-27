1da0deb9d6893d55cab0798d54bef5e334a7bc7e
Natalie Schroiff <natalie.schroiff@outlook.com>
2025-04-27 10:24:38 +0200

    #64: added javadoc for interpret exception

--- src/main/java/itf221/gvi/boom/exceptions/InterpretException.java
+++ src/main/java/itf221/gvi/boom/exceptions/InterpretException.java
@@ -1,5 +1,8 @@
 package itf221.gvi.boom.exceptions;
 
+/**
+ * Exception thrown to indicate an error occurred during interpretation or parsing of data.
+ */
 public class InterpretException extends RuntimeException {
     public InterpretException(String message) {
         super(message);
