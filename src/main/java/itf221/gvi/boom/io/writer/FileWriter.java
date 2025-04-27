f720031ad20f087bb570875bf7ad90404b2480f7
Natalie Schroiff <natalie.schroiff@outlook.com>
2025-04-27 10:05:56 +0200

    #64: added javadoc for file writer interface

--- src/main/java/itf221/gvi/boom/io/writer/FileWriter.java
+++ src/main/java/itf221/gvi/boom/io/writer/FileWriter.java
@@ -8,10 +8,32 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
 
+/**
+ * Interface for writing data into files.
+ */
 public interface FileWriter {
+
+    /**
+     * Writes a student plan to the specified file path.
+     * @param path the file path to write to.
+     * @param students the list of students.
+     * @throws IOException if an I/O error occurs.
+     */
     void writeStudentPlan(Path path, List<Student> students) throws IOException;
 
+    /**
+     * Writes a presentation attendance list to the specified file path.
+     * @param path the file path to write to.
+     * @param presentations the list of planned presentations.
+     * @throws IOException if an I/O error occurs.
+     */
     void writePresentationAttendance(Path path, List<PlannedPresentation> presentations) throws IOException;
 
+    /**
+     * Writes a room timetable plan to the specified file path.
+     * @param path the file path to write to.
+     * @param presentations the list of offered presentations.
+     * @throws IOException if an I/O error occurs.
+     */
     void writeRoomTimetable(Path path, List<OfferedPresentation> presentations) throws IOException;
 }
