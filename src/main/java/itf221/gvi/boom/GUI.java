82b4ef2de7b64487708651859ed20ad1f6432d0c
Natalie Schroiff <natalie.schroiff@outlook.com>
2025-04-27 10:46:21 +0200

    #64: added javadoc for gui class

--- src/main/java/itf221/gvi/boom/GUI.java
+++ src/main/java/itf221/gvi/boom/GUI.java
@@ -6,7 +6,16 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+/**
+ * Main JavaFX application class for launching the BOOM user interface.
+ */
 public class GUI extends Application {
+    /**
+     * Starts the JavaFX application by loading the main view and setting up the primary stage.
+     *
+     * @param stage the primary stage for this application
+     * @throws Exception if an error occurs during loading of the FXML or resources
+     */
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/itf221/gvi/boom/fxml/main-view.fxml"));
