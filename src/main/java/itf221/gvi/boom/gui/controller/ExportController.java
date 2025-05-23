de434b8b29cfe8b8b651796d341a41a2c093f556
Benny mahaut <bennymahaut@live.de>
2025-05-23 08:44:00 +0200

    #85: changed download directory to directory chooser for excel downloads

--- src/main/java/itf221/gvi/boom/gui/controller/ExportController.java
+++ src/main/java/itf221/gvi/boom/gui/controller/ExportController.java
@@ -1,11 +1,15 @@
 package itf221.gvi.boom.gui.controller;
 
 import itf221.gvi.boom.io.writer.XlsxWriter;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Button;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
 
-import java.awt.*;
+import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
@@ -14,6 +18,8 @@ import java.nio.file.Path;
  * Controller class for managing the export view of the application.
  */
 public class ExportController {
+    @FXML
+    Button downloadLZ_button, downloadAL_button, downloadRZL_button;
 
     @FXML
     private AnchorPane toastPane;
@@ -22,7 +28,6 @@ public class ExportController {
     private Label completionScoreToast;
 
     XlsxWriter xlsxWriter = new XlsxWriter();
-    Path downloadPath = MainController.getDownloadFolderPath();
 
     /**
      * Called automatically after the FXML is loaded.
@@ -31,33 +36,6 @@ public class ExportController {
     public void initialize() {
         showSuccessToast();
         completionScoreToast.setText("✔ Erfolg! Erfüllungsscore: " + LoadingController.completionScore + "%");
-    }
-
-    /**
-     * Method to download student plan ("Laufzettel").
-     * @throws IOException if an I/O error occurs.
-     */
-    @FXML
-    private void downloadLZ() throws IOException {
-        xlsxWriter.writeStudentPlan(downloadPath, LoadingController.boomData.getStudents());
-    }
-
-    /**
-     * Method to download attendance list ("Anwesenheitsliste").
-     * @throws IOException if an I/O error occurs.
-     */
-    @FXML
-    private void downloadAL() throws IOException {
-        xlsxWriter.writePresentationAttendance(downloadPath, LoadingController.boomData.getAllPlannedPresentations());
-    }
-
-    /**
-     * Method to download room timetable plan ("Raum-/Zeitpläne-Liste").
-     * @throws IOException if an I/O error occurs.
-     */
-    @FXML
-    private void downloadRZL() throws IOException {
-        xlsxWriter.writeRoomTimetable(downloadPath, LoadingController.boomData.getAllOfferedPresentations());
     }
 
     /**
@@ -85,6 +63,34 @@ public class ExportController {
             Desktop.getDesktop().open(pdfFile);
         } catch (IOException e) {
             System.out.println("Can't find documentation");
+        }
+    }
+
+    /**
+     * Opens a directory chooser to choose the download-location for the excel files
+     * @param event
+     * @throws IOException
+     */
+    @FXML
+    private void openDirectoryChooser(ActionEvent event) throws IOException {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Select Download Location");
+
+        File selectedDirectory = directoryChooser.showDialog(new Stage());
+
+        if (selectedDirectory != null) {
+            Path downloadPath = selectedDirectory.toPath();
+            Object source = event.getSource();
+            System.out.println("Selected directory: " + selectedDirectory.getAbsolutePath());
+            if (source == downloadLZ_button) {
+                xlsxWriter.writeStudentPlan(downloadPath, LoadingController.boomData.getStudents());
+            } else if (source == downloadAL_button) {
+                xlsxWriter.writePresentationAttendance(downloadPath, LoadingController.boomData.getAllPlannedPresentations());
+            } else if (source == downloadRZL_button) {
+                xlsxWriter.writeRoomTimetable(downloadPath, LoadingController.boomData.getAllOfferedPresentations());
+            }
+        } else {
+            System.out.println("Location selection canceled.");
         }
     }
 }
